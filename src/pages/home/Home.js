43161e935c176b30435b86ac2ab10edaaaef8db8
chengdh <cheng.donghui@gmail.com>
2018-12-13 20:37:02 +0800

    android run successs

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, WebView, Platform } from 'react-native'
+import { StyleSheet, Text, View, WebView, Platform,BackHandler } from 'react-native'
 
 export default class Home extends Component {
 
